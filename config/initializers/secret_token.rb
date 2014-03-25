0898ac06a9009c910a3d3410b63003ce756f1b86
7rrf <sm7rrfmail.com>
2014-03-25 09:16:07 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '86a7f2efe1614e2bab650fcddcc85c086b583b21ccb0ed6cc82679b8829fe4d4573e1217820678c09f3c6af617c7852fadcca8afbd7824004aedde831a5c660f'
